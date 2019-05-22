96e3356bba164d1271856cbbdf9c8fa855d9e389
twong540 <twong@reliaslearning.com>
2019-05-22 13:05:04 -0400

    .Net Framework 4.6.1 Unit Tests

---
+++ PrimeService.Framework461Tests/PrimeService_IsPrimeShould.cs
@@ -0,0 +1,38 @@
+﻿using Prime.Services;
+using Xunit;
+
+namespace Prime.Framework461UnitTests.Services
+{
+    public class PrimeService_IsPrimeShould
+    {
+        private readonly PrimeService _primeService;
+        private readonly OtherService _otherService;
+
+        public PrimeService_IsPrimeShould()
+        {
+            _primeService = new PrimeService();
+            _otherService = new OtherService();
+        }
+
+        [Fact]
+        public void ReturnFalseGivenValueOf1()
+        {
+            var result = _primeService.IsPrime(1);
+
+            Assert.False(result, "1 should not be prime");
+        }
+
+        [Fact]
+        public void Test01(){
+            int n = _otherService.AddANumberToSeven(5);
+            Assert.Equal(n,5+7);
+        }
+
+
+        [Fact]
+        public void Test02(){
+            string test = _otherService.GenerateString(10);
+            Assert.Equal(test.Length,10);
+        }
+    }
+}
---
+++ PrimeService.Framework461Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PrimeService.Framework461Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PrimeService.Framework461Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0e3533dd-e5e0-4595-91ce-591a66a14c92")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

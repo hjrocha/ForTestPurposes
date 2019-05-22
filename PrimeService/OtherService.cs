1541bac3539e19b3c505ff8c2d8578cfabb054bf
Helio Rocha <hjrocha@gmail.com>
2019-05-22 10:26:39 +0100

    add other service

---
+++ PrimeService/OtherService.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Prime.Services {
+    public class OtherService {
+         public int AddANumberToSeven(int number)
+        {
+            return number +7;
+        }
+
+        public string GenerateString(int length){
+            return "";
+        }
+    }
+}
